4a52de534992cabcf67b10896b126d4bf24f7be4
accinn <aylinpt02@gmail.com>
2025-06-04 12:29:14 -0300

    add: ejercicio 2

--- PracticaProfesional/src/Ejemplos2/Fibonacci.java
+++ PracticaProfesional/src/Ejemplos2/Fibonacci.java
@@ -7,7 +7,11 @@ public class Fibonacci{
 		int rec1 = 0;
 		int rec2 = 1;
 		
-		
+        for (int i = 2; i <= numero; i++) {
+            int resultado = rec1 + rec2;
+            rec1 = rec2;
+            rec2 = resultado;
+        }
 		return numero;
 		
 	}
